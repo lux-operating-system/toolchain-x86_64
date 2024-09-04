6cd0c48f01905fe3b20da5ce2e599e8c256d405e
jewelcodes <omarmelghoul01@gmail.com>
2024-09-04 13:36:44 -0400

    change x86_64 base address to 4 MB

--- emulparams/elf_x86_64_lux.sh
+++ emulparams/elf_x86_64_lux.sh
@@ -1,5 +1,5 @@
 source_sh ${srcdir}/emulparams/elf_x86_64.sh
-TEXT_START_ADDR=0x40000000   # 1 GB
+TEXT_START_ADDR=0x400000    # 4 MB
 GENERATE_SHLIB_SCRIPT="no"  # TODO: these will be changed after implementing dynamic linking
 GENERATE_PIE_SCRIPT="no"
 MAXPAGESIZE=0x1000
